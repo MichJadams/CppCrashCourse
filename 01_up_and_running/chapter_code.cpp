c53b8866860078cd44ee1eccf81b15146c1b1edb
Michaela Adams <mjadams@fastmail.com>
2025-05-13 13:39:20 -0400

    a program that prints the results of applying step_function

--- 01_up_and_running/chapter_code.cpp
+++ 01_up_and_running/chapter_code.cpp
@@ -1,20 +1,33 @@
 #include <cstdio>
 
-int step_function(int x){
-    int result = 0; 
+int step_function(int x)
+{
+    int result = 0;
 
-    if(x < 0) {
+    if (x < 0)
+    {
         result = -1;
-    } else if (x > 0) {
-        result = 1; 
+    }
+    else if (x > 0)
+    {
+        result = 1;
     }
     return result;
 }
 
-int main() {
-   int value1 = step_function(100);
-   int value2 = step_function(0);
-   int value3 = step_function(-10);
+int main()
+{
+    int num1 = 43;
+    int result1 = step_function(num1);
 
-   printf("Value 1/; %d, value 2: %d, value 3: %d", value1, value2, value3);
+    int num2 = 1; 
+    int result2 = step_function(num2);
+    
+    int num3 = -32454;
+    int result3 = step_function(num3);
+
+    printf("num1: %d, step %d", num1, result1);
+    printf("num1: %d, step %d", num2, result2);
+    printf("num1: %d, step %d", num3, result3);
+
 }
