ac32acd4d8e2f0d088e4894d6be876b12243e175
Michaela Adams <mjadams@fastmail.com>
2025-05-13 13:36:38 -0400

    printf formate specifiers

--- 01_up_and_running/chapter_code.cpp
+++ 01_up_and_running/chapter_code.cpp
@@ -1,8 +1,20 @@
 #include <cstdio>
 
+int step_function(int x){
+    int result = 0; 
+
+    if(x < 0) {
+        result = -1;
+    } else if (x > 0) {
+        result = 1; 
+    }
+    return result;
+}
+
 int main() {
-   int x = -10; 
-   if(x > 0) printf("Positive");
-   else if(x < 0) printf("Negative");
-   else printf("Zero."); 
+   int value1 = step_function(100);
+   int value2 = step_function(0);
+   int value3 = step_function(-10);
+
+   printf("Value 1/; %d, value 2: %d, value 3: %d", value1, value2, value3);
 }
