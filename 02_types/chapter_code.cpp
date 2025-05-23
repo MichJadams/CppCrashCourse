484ad7178414b5fc732a0f79456e06118538c15e
Michaela Adams <mjadams@fastmail.com>
2025-05-23 16:58:00 -0400

    Playing with Unions
    I think maybe there is potential of unions being a really good teaching tool for
    myself. For example if I want to see the underlying decimal values of a variable
    I can make a union and have one of the values be int and that can act as a sort
    of filter over the other type (maybe a sting or whatever).

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -1,16 +1,25 @@
 #include <cstdio>
+#include <cstddef>
 
-struct Book {
-  char name[256];
-  int year;
-  int pages;
-  bool hardcover;
+union Variant {
+  char string[10];
+  int integer;
+  double floating_point;
+  std::byte bytes[10]; 
 };
 
 int main(){
 
-  Book neuromancer;
-  neuromancer.pages = 271;
-  printf("Neuromancer has %s pages.", neuromancer.name); 
+  Variant v;
+  v.integer = 42;
+  printf("as bytes: %s\n", v.bytes);
+
+  printf("The Answer: %d\n", v.integer);
+
+  v.floating_point = 2.7182818284;
+  printf("Euler number e: %f\n", v.floating_point);
+  
+  printf("as bytes: %b\n", v.bytes);
+  printf("A dumpster fire: %d\n", v.string);
 }
 
