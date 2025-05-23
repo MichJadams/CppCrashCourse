60a9d99efd19495c191378ba134655fa6c84af02
Michaela Adams <mjadams@fastmail.com>
2025-05-23 12:15:46 -0400

    Listing 2-6, comparison operators

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -3,10 +3,12 @@
 
 int main(){
 
-  bool b1 = true;
-  bool b2 = false;
-
-  printf("%d %d \n", b1, b2);
+  printf("7 ==7: %d \n", 7 == 7);
+  printf("7 != 7: %d\n", 7 != 7);
+  printf("10 > 20: %d\n", 10 > 20);
+  printf("10>= 20: %d\n", 10 >= 20);
+  printf("10 < 20: %d\n", 10 < 20);
+  printf("20 <= 20: %d\n", 20 <=20);
   
 }
 
