f5d60492ec4b4926f6c9a39d8e2235660b39cdc7
Michaela Adams <mjadams@fastmail.com>
2025-05-23 15:35:47 -0400

    An interesting Footgun?
    This is another example of something that I have not run into with other
    programming languages.
    
    Here, because char holder[] is assigned to an int, it is not a null terminated
    string ( such as { 'h', 'i', '\0'}). printf is expecting a null terminated
    string. So when it starts reading holder, it keeps reading until it hits a null
    byte. This leads to undefined behavior, or in my case, the letter a being
    printed twice.
    
    This code compiles. But there is a compiler warning of a "narrowing
    conversation" of 'letter' from 'int' to 'char'.

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -3,18 +3,10 @@
 
 int main(){
 
-  char alphabet[27];
 
   int letter = 97;
-  for(int i = 0; i < 26; i ++){
-    alphabet[i] = letter;
-    letter ++;
-  }
-  printf("alphabeter \n %s", alphabet);
-
-  for (int i =0; i < 26; i ++){
-    alphabet[i] = i + 65;
-  }
-  printf("upper case \n %s", alphabet);
+  char holder[] = {letter};
+ 
+  printf("upper case \n %c", holder);
 }
 
