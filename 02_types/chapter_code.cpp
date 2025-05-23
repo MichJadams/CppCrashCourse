e9c6376bd847798823daa0b32ced7a8dd0ab4082
Michaela Adams <mjadams@fastmail.com>
2025-05-23 15:08:28 -0400

    Changing an element in an array
    listing 2-9

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -1,20 +1,14 @@
 #include <cstdio>
-#include <cstddef>
+
 
 int main(){
-  size_t size_c = sizeof(char);
-  printf("char: %zu\n", size_c);
+  int arr[] = { 1, 2, 3, 4, };
+  printf("The third element is %d.\n", arr[2]);
 
-  size_t size_s = sizeof(short);
-  printf("short: %zu\n", size_s);
+  arr[2] = 100;
 
-  size_t size_i = sizeof(int);
-  printf("int: %zu\n", size_i);
+  printf("The third element is %d. \n", arr[2]);
+  
 
-  size_t size_l = sizeof(long);
-  printf("long: %zu\n", size_l);
-
-  size_t size_ll = sizeof(long long);
-  printf("long long: %zu\n", size_ll);
 }
 
