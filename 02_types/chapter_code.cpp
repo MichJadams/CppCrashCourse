84402ab72dfaf9b062be64c235fd6937a34adb3a
Michaela Adams <mjadams@fastmail.com>
2025-05-23 12:08:42 -0400

    Listing 2-5, bool Variables

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -1,19 +1,12 @@
 #include <cstdio>
-#include <iostream>
+
 
 int main(){
-  char x = 'H';
-  char32_t y = U'\U0001F37A';
 
-  const char* emoji = "other things ? \xF0\x9F\x98\x80";
+  bool b1 = true;
+  bool b2 = false;
 
-  printf(" is this an emoji? %s", emoji);
-
-  std::cout << " unicode? " << static_cast<int>(y) << std::endl;
-  printf("Windows start \x41 with %c %b. \n", x,y);
-
-  printf("this is the size difference %d %d", sizeof(x), sizeof(y));
-  
+  printf("%d %d \n", b1, b2);
   
 }
 
