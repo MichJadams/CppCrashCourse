c0d578525398b6bf35b5c60d673d5109c13b2592
Michaela Adams <mjadams@fastmail.com>
2025-05-23 16:40:16 -0400

    Printing an Enum's Name
    There is no way in C++ to print and enums name because the symbols are discarded
    during compiltion. However, online there are lots of helpful substackers
    pointing out that maybe you should use a Map, or an array of string or what
    ever.

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -1,21 +1,19 @@
 #include <cstdio>
 
+// example from the book uses Neal Stephenson's Seveneves
+enum class Race {
+  Dinan,
+  Teklan,
+  Ivyn,
+  Moiran,
+  Camite,
+  Julian,
+  Aidan
+};
 
 int main(){
+  Race langobard_race = Race::Aidan; 
 
-
-  // debugger commands for setting values manually
-  // run
-  // break *line number* (do the lin that the print statement is on)
-  // print &holder
-  //  x/10cb &holder <-- this one prints the memory around the value that holder is pointing at
-  // set {char}0x5fe9c = 0x64
-  // or
-  // set {char}0x5fe9c = 100
-  // replace 0x5fe9c with your own memory addresses. 
-  int letter = 97;
-  char holder[] = {letter};
- 
-  printf("upper case \n %s", holder);
+  printf("how do you print enums? %d", Race::Dinan);
 }
 
