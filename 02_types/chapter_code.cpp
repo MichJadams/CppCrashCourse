ccaeef55a7ea6d269596558b4265f595bf97e8b9
Michaela Adams <mjadams@fastmail.com>
2025-05-23 17:02:54 -0400

    Adding Functions To Structs
    I think the difference between a struct and a class is really interesting. They
    seem so similar, with such a minor semantic difference I'm suprised a langauge
    as supposidly "terse" as C++ has both. I suppose it's probably for compatibility reasons.

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -1,25 +1,18 @@
 #include <cstdio>
 #include <cstddef>
 
-union Variant {
-  char string[10];
-  int integer;
-  double floating_point;
-  std::byte bytes[10]; 
+struct ClockOfTheLongNow {
+  void add_year(){
+    year++;
+  }
+  int year;
 };
 
 int main(){
 
-  Variant v;
-  v.integer = 42;
-  printf("as bytes: %s\n", v.bytes);
-
-  printf("The Answer: %d\n", v.integer);
-
-  v.floating_point = 2.7182818284;
-  printf("Euler number e: %f\n", v.floating_point);
-  
-  printf("as bytes: %b\n", v.bytes);
-  printf("A dumpster fire: %d\n", v.string);
+  ClockOfTheLongNow clock;
+  clock.year = 2017;
+  clock.add_year();
+  printf("year %d\n", clock.year);
 }
 
