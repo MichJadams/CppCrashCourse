de54e73fdd11ba3d8b03268f7eaa4ddc8093c96d
Michaela Adams <mjadams@fastmail.com>
2025-05-29 09:06:57 -0400

    Initializing Members
    listing 2-21, just making use of some methods!

--- 02_types/chapter_code.cpp
+++ 02_types/chapter_code.cpp
@@ -19,26 +19,15 @@ private:
   int year;
 };
 
-class ClassyLongNow {
-  int year;
-public:
-  void set_year(int new_year){
-    if(new_year < 2019) return false;
-    year = new_year;
-    return true;
-  }
-  bool add_year(){
-    year++;
-  }
-  int get_year(){
-    return year;
-  }
-}
 
 int main(){
 
   ClockOfTheLongNow clock;
-  clock.set_year(2020);
+  if(!clock.set_year(2018)){
+    clock.set_year(2019);
+  }
+      
+ 
   clock.add_year();
   printf("year %d\n", clock.get_year());
 }
