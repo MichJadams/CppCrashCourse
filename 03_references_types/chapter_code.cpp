52f118b97ab337ba40c2ceabbfd852d5c8f164bc
Michaela Adams <mjadams@fastmail.com>
2025-06-06 14:58:33 -0400

    Pointer Arithmetic!
    Still in the land of strange sounds and words, listing 3-4

--- 03_references_types/chapter_code.cpp
+++ 03_references_types/chapter_code.cpp
@@ -1,23 +1,15 @@
 #include <cstdio>
 
-struct ClockOfTheLongNow{
-  ClockOfTheLongNow(){}
-  ClockOfTheLongNow(int year): year{year}{}
-  int get_year() {
-    return year;
-  }
-  void set_year(int year) {
-      this->year = year;
-  }
-private:
-  int year;
+struct College{
+  char name[256];
 };
 
+void print_name(College* college){
+  printf("%s College\n", (*college).name);
+}
+
 int main(){
-  ClockOfTheLongNow clock;
-  ClockOfTheLongNow* clock_ptr = &clock;
-  clock_ptr->set_year(2022);
-  printf("address of the clock: %p\n", clock_ptr);
-  printf("Value of clock's year: %d", clock_ptr->get_year());
+  College best_colleges[]{"Princeton", "University", "other", "No college"};
+  print_name(best_colleges + 2);
 }
 
