84ba4c66f5f43d19c5153140b09697987fdd9bff
Michaela Adams <mjadams@fastmail.com>
2025-06-08 07:29:59 -0400

    3-4 Using Auto
    I'm not totally sure how to use printf with auto. At this point it
    sems like I still need to declare a formatter. I suppose with cout and
    cin eventually I won't need to do that and I can really take advantage
    of auto? But for now I'll just have to manuall change it from %d to %s
    or whatever.

--- 03_references_types/exercises.cpp
+++ 03_references_types/exercises.cpp
@@ -1,63 +1,19 @@
 #include <cstdio>
-#include <exception>
 
-// Read about CVE-2001-0500
-// 3-3 add a previous to the linked list making it a doubly linked list
-// and add an insert_before to element
-// traverse the list back and forth using two seperate for loops 
 
-struct Element
-{
-  int data; 
-  void insert_after(Element* next_element)
-  {
-    next_element->next = next; 
-    next = next_element;
-    next_element->previous = this;
-   }
-
-  void insert_before(Element* new_element)
-  {
-    new_element->previous = previous;
-    previous = new_element;
-    new_element->next = this;
-    
-  }
-  Element* next;
-  Element* previous;
-  
-};
-
-void print_linked_list_forward(Element* start)
-{
-  Element* current_element = start;
-  while(current_element)
-    {
-      printf("element data: %d\n", current_element->data);
-      current_element = current_element->next;
-    }
-};
-
-void print_linked_list_backward(Element* end)
-{
-  Element* current_element = end;
-  while(current_element)
-    {
-      printf("Element data: %d\n", current_element->data);
-      current_element = current_element->previous;
-    }
-  
-}
+// 3-4 Reimplement listing 3-11 using no explicit types (using auto)
 
 int main()
 {
-  Element one{10};
-  Element two{20};
-  Element three{30};
+  auto original = "hello";
+  auto& original_ref = original;
+  printf("original: %s\n", original);
+  printf("Reference: %s\n", original_ref);
 
-  one.insert_after(&two);
-  two.insert_after(&three);
-
-  print_linked_list_forward(&one);
-  print_linked_list_backward(&three);
+  printf("----------------\n");
+  auto new_value = "different";
+  original_ref = new_value;
+  printf("Original: %s\n", original);
+  printf("New Value: %s\n", new_value);
+  printf("Reference: %s\n", original_ref);
 }
