cd4007940cc7887d34cce8ddc0f118aa62d04301
Michaela Adams <mjadams@fastmail.com>
2025-06-18 12:28:12 -0400

    "Moving" on to Copy Semantics
    heh.

--- 04_the_object_life_cycling/chapter_code.cpp
+++ 04_the_object_life_cycling/chapter_code.cpp
@@ -1,104 +1,15 @@
-
 #include <cstdio>
-#include <stdexcept>
-#include <cstdio>
-#include <cstring>
-struct Tracer
+
+int add_one_to(int x)
 {
-  Tracer(const char* name) :name {name}
-  {
-    printf("%s constructed.\n", name);
-  }
+  x++;
+  return x;
+}
 
-  ~Tracer()
-  {
-    printf("%s destructed.\n", name);
-  }
-
-private:
-  const char * const name; //so many consts
-};
-
-  static Tracer t1 { "static variable" };
-thread_local Tracer t2{ "thread-local variable"};
-
-struct Groucho
-{
-  void forget(int x)
-  {
-    if(x == 0xFACE)
-      {
-	throw std::logic_error{"It's not about who wrong""it's not aobut who right"};
-      }
-    printf("Forgot 0x%x\n", x);
-  }
-};
-
-struct CyberdyneSeries800
-{
-  CyberdyneSeries800()
-  {
-    printf("I'm a friend of sarah connor.");
-  }
-
-  ~CyberdyneSeries800()
-  {
-    throw std::runtime_error{"I'll be back!"};
-  }
-
-};
-
-
-struct SimpleString
-{
-  SimpleString(int max_size): max_size{max_size}, length{}
-  {
-    buffer = new char[max_size];
-    buffer[0] = 0; 
-  }
-
-  void append_line(const char* x)
-  {
-    int x_len = strlen(x);
-    std::strncpy(buffer + length, x, max_size - length);
-    length += x_len;
-    buffer[length++] = '\n';
-    buffer[length] = 0; 
-
-  }
-
-  void print()
-  {
-    printf("%s", buffer);
-  }
-
-private:
-  int max_size;
-  int length;
-  char* buffer;
-};
-
-struct SimpleStringOwner
-{
-  SimeplStringOwner(const char* x): string{10}
-  {
-    string.append_line(x);
-  }
-  ~SimpleStringOwner()
-  {
-    string.print("about to destroy");
-  }
-private:
-  SimpleString string; 
-};
 int main()
 {
-
-  SimpleString string {115};
-  string.append_line("hey!");
-  string.append_line("nothing but the rain");
-  string.print();
-		
- 
+  auto original = 1;
+  auto result = add_one_to(original);
+  printf("original %d; result: %d", original, result); 
 }
 	 
