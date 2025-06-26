d1a7829cb04d7dd53ca82100abdf8598e1834346
Michaela Adams <mjadams@fastmail.com>
2025-06-26 11:07:24 -0400

    Listing 5-2 - This is how banks work
    I forgot to use break in my switch statement which is embarrassing but
    I did figure it out which was gratifying.

--- 05_runtime_polymorphism/chapter_code.cpp
+++ 05_runtime_polymorphism/chapter_code.cpp
@@ -1,7 +1,74 @@
 #include <cstdio>
+#include <stdexcept>
+
+struct ConsoleLogger
+{
+  static void log_transfer(long from, long to, double amount)
+  {
+    printf("%ld -> %ld: %f\n", from, to, amount);
+
+  }
+};
+
+
+
+enum class LoggerType
+  {
+   Console,
+   File
+  };
+
+
+struct FileLogger
+{
+  static void log_transfer(long from, long to, double amount)
+  {
+    printf("[cons] %ld -> %ld: %f\n", from, to, amount);
+  }
+};
+
+struct Bank
+{
+  Bank(): type { LoggerType::Console }{}
+
+  void set_logger(LoggerType new_type)
+  {
+    type = new_type;
+  }
+  void make_transfer(long from, long to, double amount)
+  {
+    switch(type)
+      {
+      case(LoggerType::File) :
+	{
+	  FileLogger::log_transfer(from, to, amount);
+	  break;
+      	}
+      case(LoggerType::Console) :
+	{
+	  ConsoleLogger::log_transfer(from, to, amount);
+	  break;
+	}
+      default:
+	{
+	  throw std::logic_error("Unknown logger type");
+	}
+			  
+      };
+  }
+  LoggerType type; 
+};
+
 
 int main()
 {
-  printf("inside runtime polymohphism\n");
+  Bank bank;
+  bank.make_transfer(100, 200, 49.95);
+  bank.make_transfer(2000, 4000, 20.00);
+
+  bank.set_logger(LoggerType::File);
+
+  bank.make_transfer(7777, 8390, 4.4);
+
 }
  
