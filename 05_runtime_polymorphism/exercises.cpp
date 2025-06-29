b19f1431686ff6daffd2da3df8faaeb315062912
Michaela Adams <mjadams@fastmail.com>
2025-06-29 18:31:45 -0400

    Exercise 5-1
    Created a interface, also has to create an additional Account class,
    which I'm just gonna assume is fine.

--- 05_runtime_polymorphism/exercises.cpp
+++ 05_runtime_polymorphism/exercises.cpp
@@ -1,3 +1,23 @@
+#include <cstdio>
+
+// 5-1 Design an interface called AccountDatabase
+// that can retrieve and se tamounts in bank accounts
+// identified by a long id
+struct Account
+{
+  long id;
+  float initial_amount;
+};
+
+struct AccountDatabase
+{
+  virtual void add_new_account(long id, float initial_amount)=0;
+  virtual void add_money(long id, float amount) = 0;
+  virtual void remove_money(long id, float amount) =0; 
+}
 
 
-int main(){}
+int main()
+{
+
+}
