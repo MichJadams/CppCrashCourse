7125f338e7ce13d4b0702cf155fb4e95a0642f66
Michaela Adams <mjadams@fastmail.com>
2025-07-07 09:34:50 -0400

    Listing 6-1!
    Here we goooooo! It's been a minute so hopefully I haven't forgotten
    everything.

---
+++ 06_compiletime_polymorphism/chapter_code.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+template <typename x, typename y, typename z>
+struct MyTemplateClass
+{
+  x foo(&Y);
+private:
+  Z* member;
+}
+
+
+int main()
+{
+  printf("hello world\n");
+}
