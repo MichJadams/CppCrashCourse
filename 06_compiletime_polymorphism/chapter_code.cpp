68754f9d65e83841f863ceee16b0396cd4c81722
Michaela Adams <mjadams@fastmail.com>
2025-07-14 15:39:04 -0400

    Listing 6-29 Using static_assert Expressions
    Okay, I think I might just be a static_assert baddie and use them
    instead of concepts. They feel much simpler unless you are building
    types on types. But even then a static_assert is much more explicit.

--- 06_compiletime_polymorphism/chapter_code.cpp
+++ 06_compiletime_polymorphism/chapter_code.cpp
@@ -1,16 +1,13 @@
 #include <cstdio>
-#include <utility>
-#include <type_traits>
-#include <concepts>
+#include <utility> 
+
 
 template<typename T>
-requires requires(T a, T b)
-{
-  {a + b} -> std::same_as<T>;
-  {a / size_t{1}} -> std::same_as<T>;
-}
 T mean(T* list, size_t length)
 {
+  // I actually like this a lot 
+  static_assert(std::is_default_constructible<T>(), "type must be default constructible.");
+  static_assert(std::is_arithmetic<T>(), "type must support addition and division."); 
   T result{};
   for(int i{}; i< length; i++)
     {
