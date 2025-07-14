d0f59607a15255dab97c5ba899b349c55c7a134f
Michaela Adams <mjadams@fastmail.com>
2025-07-14 12:31:12 -0400

    Listing 6-11
    I took a bit of a break and it feels good to be back! Templates feel
    like an understandable concept. Very similar to generics in C# so
    feeling strong there. It is interesting that one problem I ran into
    was that passing a result into print you don't actually know what type
    it is.
    
    I guess to solve this I could wrap printf in my own template function
    that would have a switch statement that would use the correct
    %(string templating function).

--- 06_compiletime_polymorphism/chapter_code.cpp
+++ 06_compiletime_polymorphism/chapter_code.cpp
@@ -11,17 +11,29 @@ To narrow_cast(From value)
   return converted;
 }
 
+template<typename V>
+V mean(const V* values, size_t length)
+{
+  V result{};
+  for(size_t i{}; i<length; i++)
+    {
+      result += values[i];
+    }
+  return result / length;
+}
+
 int main()
 {
-  int perfect {496};
-  const auto perfect_short = narrow_cast<short>(perfect); 
-  try
-    {
-      int cyclib {142857};
-      const auto cyclib_short = narrow_cast<short>(cyclib);
-    }
-  catch(std::runtime_error& e)
-    {
-      printf("Error: %s", e.what());
-    }
+  const double nums_d[] {1.0, 2.0, 3.0, 4.0};
+  const auto result1 = mean<double>(nums_d, 4);
+  printf("double: %f\n", result1);
+
+  const float nums_f[] {1.0f, 2.0f, 3.0f, 4.0f};
+  const auto result2 = mean<float>(nums_f, 4);
+  printf("float: %f\n", result2);
+
+  const size_t nums_c[] {1,2,3,4};
+  const auto result3 = mean<size_t>(nums_c, 4);
+  printf("size_t: %zu\n", result3); // the book suggests using zu, I don't acutally know what
+  // that does ??
 }
