502f9759f420d6ab333f4f45c1cd2b9ee5eb4a74
Michaela Adams <mjadams@fastmail.com>
2025-07-14 15:06:08 -0400

    Listing 6-21, using concepts
    Concepts pairs with requirements and constraints. So one technical
    thing here is that float += float does not return float, but instead
    float&.
    
    This is a cool subtlety and one that I honestly don't totally
    understand right now. Basically it is because the result of lhs += rhs
    is a reference to the left-hand operand, not a new float!
    
    A += b modifies a in place, and returns a reference to a. This allows
    for chaining.
    
    a += b += c
    
    If this returned a value, you could not chain like this.
    
    ChatGPT wants me to image that because this statement returns a
    reference to a (in other words &a), I can continue to "pour things into
    the box". Which I think for right now is just something I'm going to
    have to memorize rather than perfectly understand.

--- 06_compiletime_polymorphism/chapter_code.cpp
+++ 06_compiletime_polymorphism/chapter_code.cpp
@@ -1,84 +1,41 @@
 #include <cstdio>
 #include <utility>
+#include <type_traits>
+#include <concepts>
 
-template<typename T> 
-struct SimpleUniquePointer
-{
-  SimpleUniquePointer() = default;
-  SimpleUniquePointer(T* pointer): pointer{pointer}{}
-  ~SimpleUniquePointer()
-  {
-    if(pointer) delete pointer;
-  }
-
-  // make the copy constructors not possible 
-  SimpleUniquePointer(const SimpleUniquePointer&) = delete;
-  SimpleUniquePointer& operator=(const SimpleUniquePointer&) = delete;
-
-  // make the move constructor remove the pointer from
-  // the original 
-  SimpleUniquePointer(SimpleUniquePointer&& other) noexcept
-    : pointer{other.pointer}
-  {
-    other.pointer = nullptr;
-  }
-
-  // make the move assignment operator
-  // remove the pointer from the original
-  // and return set this instance equal to the original
-  // return this
-  SimpleUniquePointer& operator=(SimpleUniquePointer&& other) noexcept
-  {
-    // this line is really key because failure to delete this pointer
-    // leaks a resource! 
-    if(pointer) delete pointer;
-
-    
-    pointer = other.pointer;
-    other.pointer = nullptr;
-    return *this;
-  }
-  T* get()
-  {
-    return pointer;
-  }
-private:
-  T* pointer;
-
-};
-
-struct Tracer
-{
-  Tracer(const char* name) : name{name}
-  {
-    printf("%s constructed \n", name);
-  }
-  ~Tracer()
-  {
-    printf("%s destucted\n", name);
-  }
-private:
-  const char* const name;
-};
-
-void consumer( SimpleUniquePointer<Tracer> consumer_ptr)
-{
-  printf("(cons) consumer_ptr: 0x%p\n", consumer_ptr.get());
-}
 
 template<typename T>
-T square(T value)
+concept Averageable = std::is_default_constructible<T>::value
+    && requires(T a, T b)
+    {
+      // this first constraint fails when working with floats
+      {a += b} -> std::same_as<T>;
+
+      // this works just fine, because it is a looser constraint
+      {a / size_t{1}} -> std::convertible_to<T>;
+    };
+
+
+
+template<Averageable T>
+T mean(T* list, size_t length)
 {
-  return value * value;
+  T result{};
+  for(int i{}; i< length; i++)
+    {
+      result += list[i];
+    }
+  return result / length; 
 }
+
+
 
 int main()
 {
-  char my_char{'T'};
-  auto result = square(my_char);
+  float test[]{1.0f,2.0f,3.0f,4.0f};
+  float result = mean(test, 4);
 
-  // I'm suprised this compiles and prints 00!!
-  printf("this is the result: %c,  %c", result, my_char); 
+  printf("mean %f\n", result);
 }
 
   
