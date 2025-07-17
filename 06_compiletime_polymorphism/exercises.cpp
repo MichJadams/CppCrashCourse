ae87102cfbc2131ac248c6f43a05808d30f8bc3f
Michaela Adams <mjadams@fastmail.com>
2025-07-17 13:33:59 -0400

    Exercise 6-5 Making Bank A Template Class
    I kinda forget exactly how Bank is supposed to look here, so this
    might not be exactly what the author had in mind. But I got some
    practice in with the template syntax which I think was the point!

--- 06_compiletime_polymorphism/exercises.cpp
+++ 06_compiletime_polymorphism/exercises.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <concepts>
-//6-4 refactor mean to accept an array rather than a pointer and length arguments. 
+//6-5 Using the example from Chater 5, make a bank template class that
+// accepts a template parameter.
+// use this type parameter as the type of an account rather than long.
+// verify that you code still works using a Bank<long> class. 
 
 template<typename T>
 struct Frequency
@@ -70,9 +73,40 @@ T mode(const T (&values)[Length])
   return f.find_most_frequent_value(); 
 }
 
+template<std::integral T>
+struct Account
+{
+  T id;
+  float initial_amount;
+};
+  
+template<typename T>
+struct Bank
+{
+  Bank(size_t max_size) : max_size{max_size}
+  {
+    accounts = new Account<T>[max_size]; 
+  }
+  
+  void add_new_account(T id,float initial_amount)
+  {
+    if(sizeof(accounts) < max_size)
+      {
+	accounts[length].id = id;
+	accounts[length].initial_amount = initial_amount;
+	length++;
+      }
+  }
+
+private:
+  Account<T>* accounts = nullptr;
+  int length = 0;
+  T account;
+  size_t max_size;
+};
+
 int main()
 {
-  int test[] = {1, 4, 5, 7, 3, 8, 2, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10};
-  int result = mode<int>(test);
-  printf("result %d \n", result);
+  Bank<long> b{10};
+  b.add_new_account(100, 100); 
 }
